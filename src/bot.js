1eb55c3cdfe4d114750d1953ffc873ba2c16cf43
KingDaniel2004 <danielelvisr@gmail.com>
2020-12-12 22:29:57 -0500

    made the bot working, finally!!!

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,31 @@
 
 require('dotenv').config();
-
-console.log("hello yes");
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+const tf = require('@tensorflow/tfjs-node')
+// Load the binding (CPU computation)
+const mobilenet = require('@tensorflow-models/mobilenet');
+// for getting the data images
+var image = require('get-image-data')
+
+var fs = require('fs'),
+    http = require('http'),
+    url = require('url'),
+    open = require('open');
+    // for getting the data images
+
+//var image = require('get-image-data')
+
+//const ml5 = require('ml5');
+//var ml5 = require('ml5');
+
+//global
+//import * as tf from '@tensorflow/tfjs-node'
+//var tf = require('@tensorflow/tfjs-node');
+//var MN = require('@tensorflow-models/mobilenet');
+//tf.setBackend('cpu');
+//tf.ready();
 
 client.login(process.env.BOTTOKEN);
 
@@ -12,13 +33,97 @@ client.on('ready', readyDiscord => {console.log("Bot is online...");});
 
 client.on('message', gotMsg);
 
-function gotMsg(msg){
-    console.log(msg.content);
-    if (msg.content === 'feddy is pro'){
-        msg.reply('no you');
-        //msg.channel.send('no you');
 
+//>= == === >== ==> >< >< >< <> <=> <===> <==>
+
+const prefix = ".";
+var MSG;
+
+function gotMsg(msg) {
+    MSG = msg;
+
+    //console.log(msg);
+    //console.log(msg.content);
+    if (!msg.content.startsWith(prefix)) return;
+
+    if (checkURL(msg.content)){
+        //msg.reply("A valid image"); 
+        var url = msg.content.substring(prefix.length);
+        console.log('\n\n\n\n image loading...\n');
+        classfy(url);
+    } else {
+        var url = msg.content.substring(prefix.length);
+        console.log('\n\n\n\n image loading...\n');
+        classfy(url);
+        //msg.reply("NO invalid image");
     }
 
 }
+
+function modelReady(){
+    console.log("Model is ready");
+}
+
+async function classfy(url){
+
+
+    /*
+    //classifer = tf.ima
+    const modle = MN.load();
+
+    const predictions = await model.classify(url);
+
+    console.log(predictions);
+    //open(url);
+    */
+
+    console.log(url);
+
+    image(url, async function (err, image) {
+        try{
+            const numChannels = 3;
+            const numPixels = image.width * image.height;
+            const values = new Int32Array(numPixels * numChannels);
+            pixels = image.data
+            for (let i = 0; i < numPixels; i++) {
+                for (let channel = 0; channel < numChannels; ++channel) {
+                    values[i * numChannels + channel] = pixels[i * 4 + channel];
+                }
+            }
+            const outShape = [image.height, image.width, numChannels];
+            const input = tf.tensor3d(values, outShape, 'int32');
+            await load(input)
+        }catch(e){
+            //functionToHandleError(e);
+        }
+    });
+
+}
+
+
+async function load(img){
+    // Load the model.
+    const model = await mobilenet.load();
+
+    // Classify the image.
+    const predictions = await model.classify(img);
+
+    var result = "Predictions: ";
+    console.log("printing... ");
+    for (var i = 0; i < predictions.length; i++){
+       console.log("printing... ");
+       result += "\n (" + i + ")  [" + (predictions[i].probability * 100).toFixed(2) + "%] " + (predictions[i].className);
+    }
+
+    MSG.reply(result)
+    console.log("output: " +result);
+
+    //console.log(predictions[0].className);
+}
+
+
+function checkURL(url) {
+    return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+}
+
 
